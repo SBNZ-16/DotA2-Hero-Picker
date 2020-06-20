4ac17b1db44f459fbf02993bacb9e279123e6876
Aleksandar Nedaković <aleksaleks123@gmail.com>
2020-06-20 14:41:35 +0200

    Make Item Calculator tests

--- DotA-HeroPicker/src/test/java/ruleTests/ItemCalculatorTests.java
+++ DotA-HeroPicker/src/test/java/ruleTests/ItemCalculatorTests.java
@@ -1,8 +1,8 @@
 package ruleTests;
 
-
 import java.util.ArrayList;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.kie.api.KieServices;
@@ -16,9 +16,9 @@ import com.heropicker.io.ItemLoader;
 import com.heropicker.model.items.ItemDatabase;
 
 public class ItemCalculatorTests {
-	
+
 	private static ItemDatabase itemDatabase;
-	
+
 	@BeforeAll
 	public static void setupHeroDatabase() {
 		try {
@@ -27,39 +27,103 @@ public class ItemCalculatorTests {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
+	// Mjollnir has Maelstrom as a component
 	@Test
-	public void mjollnirTest() {
-		
+	public void mjollnirWithMaelstromTest() {
+
 		String wantedItem = "Mjollnir";
 		ArrayList<String> boughtItems = new ArrayList<String>();
 		boughtItems.add("Maelstrom");
-		
+
 		Integer balance = getResults(boughtItems, wantedItem);
-		
-		System.out.println("Kranji balance: " + balance);
+		Integer expected = itemDatabase.getItemPrice("Mjollnir") - itemDatabase.getItemPrice("Maelstrom");
+
+		Assertions.assertEquals(expected, balance);
+	}
+
+	// Vanguard has Vitality Booster and Ring of Health as its components
+	@Test
+	public void vanguardWithAllComponentsTest() {
+
+		String wantedItem = "Vanguard";
+		ArrayList<String> boughtItems = new ArrayList<String>();
+		boughtItems.add("Vitality Booster");
+		boughtItems.add("Ring of Health");
+
+		Integer balance = getResults(boughtItems, wantedItem);
+		Integer expected = 0;
+
+		Assertions.assertEquals(expected, balance);
+	}
+
+	// Orchid Malevolence has two Oblivion Staffs as its components
+	@Test
+	public void orchidWithOblivionTest() {
+
+		String wantedItem = "Orchid Malevolence";
+		ArrayList<String> boughtItems = new ArrayList<String>();
+		boughtItems.add("Oblivion Staff");
+
+		Integer balance = getResults(boughtItems, wantedItem);
+		Integer expected = itemDatabase.getItemPrice("Orchid Malevolence")
+				- itemDatabase.getItemPrice("Oblivion Staff");
+
+		Assertions.assertEquals(expected, balance);
+	}
+
+	// Orchid Malevolence has two Oblivion Staffs as its components
+	@Test
+	public void orchidWithTwoOblivionsTest() {
+
+		String wantedItem = "Orchid Malevolence";
+		ArrayList<String> boughtItems = new ArrayList<String>();
+		boughtItems.add("Oblivion Staff");
+		boughtItems.add("Oblivion Staff");
+
+		Integer balance = getResults(boughtItems, wantedItem);
+		Integer expected = itemDatabase.getItemPrice("Orchid Malevolence")
+				- 2 * itemDatabase.getItemPrice("Oblivion Staff");
+
+		Assertions.assertEquals(expected, balance);
+	}
+
+	// Linken's Sphere has Perseverance which has Ring of Health as its component 
+	// (having both bought will reduce the price only by the price of Perseverance)
+	@Test
+	public void linkenWithPerseveranceAndRingOfHealthTest() {
+
+		String wantedItem = "Linken's Sphere";
+		ArrayList<String> boughtItems = new ArrayList<String>();
+		boughtItems.add("Perseverance");
+		boughtItems.add("Ring of Health");
+
+		Integer balance = getResults(boughtItems, wantedItem);
+		Integer expected = itemDatabase.getItemPrice("Linken's Sphere")
+				- itemDatabase.getItemPrice("Perseverance");
+
+		Assertions.assertEquals(expected, balance);
 	}
 	
 	public Integer getResults(ArrayList<String> boughtItems, String wantedItem) {
-		
+
 		ArrayList<ItemHierarchyFact> hierarchyFacts = itemDatabase.createItemHierarchyFacts(wantedItem);
 		WantedItemFact wantedItemFact = itemDatabase.createWantedItemFact(wantedItem);
 		BoughtItemCollectionFact boughtItemCollectionFact = itemDatabase.prepareBoughtItemFacts(boughtItems);
-		
+
 		KieServices ks = KieServices.Factory.get();
 		KieContainer kContainer = ks.getKieClasspathContainer();
 		KieSession kSession = kContainer.newKieSession("item-test-ksession");
-		
-		for (ItemHierarchyFact fact: hierarchyFacts) {
+
+		for (ItemHierarchyFact fact : hierarchyFacts) {
 			kSession.insert(fact);
 		}
 		kSession.insert(wantedItemFact);
 		kSession.insert(boughtItemCollectionFact);
-		
+
 		kSession.fireAllRules();
-			
-		Integer balance = (Integer)kSession.getGlobal("balance");
+
+		Integer balance = (Integer) kSession.getGlobal("balance");
 		return balance;
 	}
 
